58d0d6e12c608d917ecffd71d423e0eaa8914f42
Markus Muschol <markus.muschol@denkwerk.com>
2022-09-24 21:07:35 +0200

    Add typescript and golang solution for submarine

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -4,8 +4,25 @@ enum RSEnum {
     Baz(Vec<String>)
 }
 
+
+enum Foo<T> {
+    Bar(T)
+}
+
 fn main() {
     let mut a:Vec<i32> = vec![];
     a.push(1);
     println!("{:?}",a);
+
+
+    let foo = RSEnum::Foo(5);
+
+    if let RSEnum::Foo(value) = foo {
+
+    }
+
+    match foo{
+        RSEnum::Foo(value) => {},
+        _ => {}
+    }
 }
